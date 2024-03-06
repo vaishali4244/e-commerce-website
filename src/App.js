856d7fc7da54f8b8028bb23fc5357ca9fe785d00
VAISHALI <vaishali120895@gmail.com>
2024-03-07 04:22:15 +0530

    code edited

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 
 import { BrowserRouter } from 'react-router-dom';
 import Screen from './screens';
-import LandingPage from './screens/landingPage';
 
 function App() {
   return (
     <>
-      {/* <LandingPage/> */}
-    <BrowserRouter>
-  
-      <Screen />
-    </BrowserRouter>
+      <BrowserRouter>
+        <Screen />
+      </BrowserRouter>
     </>
   );
 }
